3a76c4213bd51316c3a02e9583bfbf6c6ee7e338
name <rnqhsrhkd@gmail.com>
2021-03-17 07:25:28 +0900

    MapManager

---
+++ Assets/01_Scripts/System/MapManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MapData;
+public class MapManager : MonoBehaviour
+{
+    CoupleInt rand;
+    Map gameWorld;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameWorld = new Map();
+        gameWorld.InitMap(MapSize.ThreebyThree, AreaSize.NinebyNine);
+        Debug.Log("Map Initialize Complete!!");
+    }
+    //void Update()
+    //{
+    //    if(Input.GetKeyDown(KeyCode.R))
+    //    {
+            
+    //    }
+    //}
+}
