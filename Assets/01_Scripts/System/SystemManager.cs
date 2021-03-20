d6d2e126db8fcce3e082f3c09673aee251d65524
name <rnqhsrhkd@gmail.com>
2021-03-20 16:35:17 +0900

    SystemManager

---
+++ Assets/01_Scripts/System/SystemManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SystemManager : MonoBehaviour
+{
+    private static SystemManager instance;
+
+    public static SystemManager Instance        //singleT
+    {
+        get
+        {
+            if (!instance)
+            {
+                instance = FindObjectOfType(typeof(SystemManager)) as SystemManager;
+
+                if (instance == null)
+                    Debug.Log("no Singleton obj");
+            }
+            return instance;
+        }
+    }
+    BaseSceneMain currentSceneMain;
+    public BaseSceneMain CurrentSceneMain
+    {
+        set
+        {
+            currentSceneMain = value;
+        }
+    }
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Debug.LogError("SystemManager is initialized twice!");
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+    void Start()
+    {
+        BaseSceneMain baseSceneMain = GameObject.FindObjectOfType<BaseSceneMain>();
+        Debug.Log("OnSceneLoaded! baseSceneMain.name = " + baseSceneMain.name);
+        SystemManager.Instance.CurrentSceneMain = baseSceneMain;
+    }
+    public T GetCurrentSceneMain<T>() where T : BaseSceneMain
+    {
+        return currentSceneMain as T;
+    }
+
+}
