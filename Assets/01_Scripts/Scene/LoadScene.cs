da2cfa64fe03ed214d3e0ca6133fc529fa69c774
name <rnqhsrhkd@gmail.com>
2021-03-20 18:28:34 +0900

    LoadScene

---
+++ Assets/01_Scripts/Scene/LoadScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadScene : MonoBehaviour
+{
+    public Slider loadingSlider;
+
+    public void LoadLevel(int _SceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(_SceneIndex));
+    }
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+
+            loadingSlider.value = progress;
+
+            yield return null;
+        }
+    }
+}
