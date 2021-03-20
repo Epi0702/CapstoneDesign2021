7d2540a7ca50ee03df8e07e6d48f48205fe96b3b
name <rnqhsrhkd@gmail.com>
2021-03-20 18:29:10 +0900

    GameData

---
+++ Assets/01_Scripts/System/GameData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SerializeField]
+public class GameData
+{
+    public string User_id = "";
+    public string PW = "";
+}
