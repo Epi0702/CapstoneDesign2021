8ab7a931f3f0c5810b96483f48f5841263e609b6
name <rnqhsrhkd@gmail.com>
2021-03-20 16:38:16 +0900

    Camera Sizing

---
+++ Assets/01_Scripts/System/CameraResolution.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraResolution : MonoBehaviour
+{
+    void Awake()
+    {
+        Camera camera = GetComponent<Camera>();
+        Rect rect = camera.rect;
+        float scaleheignt = ((float)Screen.width / Screen.height) / ((float)16 / 9);        //가로/세로
+        float scalewidth = 1f / scaleheignt;
+        if (scaleheignt < 1)
+        {
+            rect.height = scaleheignt;
+            rect.y = (1f - scaleheignt) / 2f;
+        }
+        else
+        {
+            rect.width = scalewidth;
+            rect.x = (1f - scalewidth) / 2f;
+        }
+        camera.rect = rect;
+    }
+}
