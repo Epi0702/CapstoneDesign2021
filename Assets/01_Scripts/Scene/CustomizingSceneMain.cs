75701cd4f8646060a180115c56ad633c5cc27a7e
name <rnqhsrhkd@gmail.com>
2021-03-23 14:21:50 +0900

    customizing

---
+++ Assets/01_Scripts/Scene/CustomizingSceneMain.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomizingSceneMain : BaseSceneMain
+{
+    protected override void OnAwake()
+    {
+
+    }
+    protected override void OnStart()
+    {
+
+    }
+    protected override void UpdateScene()
+    {
+
+    }
+}
