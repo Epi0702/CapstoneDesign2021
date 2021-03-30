d28425c8a7b728a1f381d26c1b433ccb83a25bb7
name <rnqhsrhkd@gmail.com>
2021-03-30 20:18:41 +0900

    PlayerPrefab

---
+++ Assets/01_Scripts/Actor/Player/PlayerCharacter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum PlayerCharacterClass
+{
+    Knight = 0,
+    Crusader = 1,
+    Swordsman = 2,
+    Monk = 3,
+    Archer = 4,
+    Ninja = 5,
+    Priest = 6,
+    Bishop = 7,
+}
+public class PlayerCharacter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/01_Scripts/Actor/Player/Playerlook.cs
+++ Assets/01_Scripts/Actor/Player/Playerlook.cs
@@ -3,24 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Experimental.U2D.Animation;
 
+
 public class Playerlook : MonoBehaviour
 {
     SpriteLibrary spriteLibrary;
-    // Start is called before the first frame update
     void Start()
     {
         spriteLibrary = this.GetComponent<SpriteLibrary>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.R))
-        {
-            //spriteLibrary.spriteLibraryAsset = Resources.Load<SpriteLibraryAsset>("Test/Acolyte Sprite Library Asset");
-            //GetSpriteLibraryAsset(Resources.Load<SpriteLibraryAsset>("Test/Acolyte Sprite Library Asset"));
-        }
-    }
     public void GetSpriteLibraryAsset(SpriteLibraryAsset _spriteLibraryAsset)
     {
         spriteLibrary.spriteLibraryAsset = _spriteLibraryAsset;
