6c7c667be8ab6231007b8acfefbbce6bdd35d0de
name <rnqhsrhkd@gmail.com>
2021-04-05 05:57:03 +0900

    Player

---
+++ Assets/01_Scripts/Actor/Player/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerState
+{
+    None,
+    NoneInRoom,
+    BattleInRoom,
+    NoneInPassage,
+    MoveForwardInPassage,
+    BattleInPassage,
+}
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    Character[] PlayerCharacter;
+
+    PlayerState playerState;
+
+    //Room currentRoom;
+    //Aisle currentAisle;
+    //Passage currentPassage;
+    private void Awake()
+    {
+        playerState = PlayerState.None;
+        //currentRoom = null;
+        //currentAisle = null;
+        //currentPassage = null;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //float num = 0;
+        //num += (1 * Time.deltaTime);
+        //Debug.Log(num);
+    }
+    void UpdateCurrentLocation()
+    {
+
+    }
+}
---
+++ Assets/01_Scripts/Scene/InGameSceneMain.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameSceneMain : BaseSceneMain
+{
+    [SerializeField]
+    MapManager mapmanager;
+    public MapManager MapManager
+    {
+        get
+        {
+            return mapmanager;
+        }
+    }
+    [SerializeField]
+    UIMapViewer uiMapViewer;
+    public UIMapViewer UIMapViewer
+    {
+        get
+        {
+            return uiMapViewer;
+        }
+    }
+    [SerializeField]
+    Player player;
+    public Player Hero
+    {
+        get
+        {
+            if (!player)
+            {
+                Debug.LogError("Main Player is not setted!");
+            }
+
+            return player;
+        }
+    }
+    protected override void OnAwake()
+    {
+        base.OnAwake();
+    }
+    protected override void OnStart()
+    {
+        base.OnStart();
+    }
+
+    protected override void UpdateScene()
+    {
+        base.UpdateScene();
+    }
+}
